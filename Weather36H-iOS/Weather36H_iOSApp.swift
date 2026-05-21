454d6a75dc6c90bb7ee5ae96ee6a14d608c6cf87
Stan Liu <shat79799@gmail.com>
2026-05-21 21:51:43 +0800

    first commit

---
+++ Weather36H-iOS/Weather36H_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather36H_iOSApp.swift
+//  Weather36H-iOS
+//
+//  Created by Stan Liu on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather36H_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather36H-iOSTests/Weather36H_iOSTests.swift
@@ -0,0 +1,18 @@
+//
+//  Weather36H_iOSTests.swift
+//  Weather36H-iOSTests
+//
+//  Created by Stan Liu on 2026/5/21.
+//
+
+import Testing
+
+struct Weather36H_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
